71cff22145f66004da7cfc1632c6eb0d05b61428
titokeene <keene@titosolutions.ph>
2025-09-22 13:33:45 +0800

    Login Test Script
    Login Test Script w/ the following test scenarios:
    Login & Logout
    Login via incorrect email
    Login via incorrect password
    
    Note: Login page is not final so basic test scripts were only written

---
+++ tests/e2e/login.spec.ts
@@ -0,0 +1,64 @@
+//Library Imports
+import { expect, test } from '@playwright/test';
+
+//Pages
+import { LoginPage } from '../../pages/login/login-page';
+import { DashboardPage } from '../../pages/content-management/dashboard-page';
+
+import { paths } from '../../variables/paths';
+
+//Components
+import { SideBar } from '../../components/auth-sidebar';
+
+//Credentials
+import { adminCredentials } from '../../variables/login';
+
+test.beforeEach(async ({ page }) => {
+    // Runs before each test and signs in each page.
+    const initLogin = new LoginPage(page);
+    await initLogin.goto();
+    await initLogin.assertElements();
+});
+
+// Login validation using correct credentials
+test("Login with valid credentials + Logout", async ({ page }) => {
+
+    //Initialize pages
+    const validLogin = new LoginPage(page);
+    const dashboardPage = new DashboardPage(page);
+    const sideBar = new SideBar(page);
+
+    await validLogin.login(
+        adminCredentials.EMAIL,
+        adminCredentials.PASSWORD
+    );
+    await dashboardPage.assertElements();
+    await expect(page).toHaveURL(process.env.TESTING_ENV! + paths.sa_dashboard);
+    await sideBar.logout();
+});
+
+// Login validation using incorrect email
+test("Login with incorrect email", async ({ page }) => {
+
+    //Initialize pages
+    const invalidEmailLogin = new LoginPage(page);
+
+    await invalidEmailLogin.login(
+        adminCredentials.EMAIL_INVALID,
+        adminCredentials.PASSWORD
+    );
+    await invalidEmailLogin.assertInvalidCredentials();
+});
+
+// Login validation using incorrect password
+test("Login with incorrect password", async ({ page }) => {
+
+    //Initialize pages
+    const invalidPWLogin = new LoginPage(page);
+
+    await invalidPWLogin.login(
+        adminCredentials.EMAIL,
+        adminCredentials.PASSWORD_INVALID
+    );
+    await invalidPWLogin.assertInvalidCredentials();
+});
