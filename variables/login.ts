df62982eb9a857be75a44260c444e267e7fb48ef
titokeene <keene@titosolutions.ph>
2025-09-22 13:30:16 +0800

    Setup of re-usable login credentials

---
+++ variables/login.ts
@@ -0,0 +1,6 @@
+export const adminCredentials  = {
+    EMAIL: "christine+admin1@titosolutions.ph",
+    EMAIL_INVALID: "invalid@titosolutions.ph",
+    PASSWORD: "123456Qwert!",
+    PASSWORD_INVALID: "invalidPassword123!",
+}
